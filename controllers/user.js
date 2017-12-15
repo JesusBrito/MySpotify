3d73101fd4bf3984d1ba5b4822f4f23545d9d84f
JesusBrito <jesus291196@gmail.com>
2017-12-14 23:03:10 -0600

    Carga de las imágenes de perfil del usuario al servidor

--- controllers/user.js
+++ controllers/user.js
@@ -74,8 +74,58 @@ function loginUser(req,res){
 	});
 }
 
+function updateUser(req, res){
+	var userId = req.params.id;
+	var update = req.body;
+
+	User.findByIdAndUpdate(userId, update, (err, userUpdated)=>{
+		if (err){
+			res.status(500).send({message:'Error al actualizar el usuario'});
+		}else if(!userUpdated){
+			res.status(404).send({message:'No se ha podido actualizar el usuario'});	
+		}else{
+			res.status(500).send({user:userUpdated});	
+		}
+	});
+}
+
+function uploadImage(req, res){
+	var userId = req.params.id;
+	var file_name = 'No subido...';
+
+	if (req.files){
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('/');
+		var file_name = file_split[2];
+
+		var ext_split = file_name.split('.');
+		var file_ext = ext_split[1];
+
+		
+		if (file_ext=='png'||file_ext=='jpg'|| file_ext=='gif'){
+
+			User.findByIdAndUpdate(userId,{image: file_name}, (err, userUpdated)=>{
+				if (err){
+					res.status(500).send({message:'Error al actualizar el usuario'});
+				}else if(!userUpdated){
+					res.status(404).send({message:'No se ha podido actualizar el usuario'});	
+				}else{
+					res.status(500).send({user:userUpdated});	
+				}
+			});
+
+		}else{
+			res.status(200).send({message: 'Extensión no valida'});
+		}
+	}else{
+		res.status(200).send({message: 'No se ha cargado ninguna imagen'});
+	}
+}
+
 module.exports= {
 	pruebas,
 	saveUser,
-	loginUser
+	loginUser,
+	updateUser,
+	uploadImage
 }
--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -15,8 +15,7 @@ exports.ensureAuth = function(req, res, next){
 				return res.status(401).send({message:'Token ha expirado'});
 			}
 		} catch(e) {
-			// statements
-			console.log(e);
+			//console.log(e);
 			return res.status(403).send({message:'Token no valido'});
 		}
 
